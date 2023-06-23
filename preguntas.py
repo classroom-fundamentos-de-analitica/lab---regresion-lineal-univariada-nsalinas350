86d702e3f51e4fd77f499978adfc6279fda55d68
nsalinas350 <80347341+nsalinas350@users.noreply.github.com>
2023-06-23 17:23:51 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -103,46 +103,46 @@ def pregunta_03():
 
 
 def pregunta_04():
-    """
-    Particionamiento del conjunto de datos usando train_test_split.
-    Complete el código presentado a continuación.
-    """
+    # """
+    # Particionamiento del conjunto de datos usando train_test_split.
+    # Complete el código presentado a continuación.
+    # """
 
-    # Importe LinearRegression
-    # Importe train_test_split
-    # Importe mean_squared_error
-    from sklearn.linear_model import LinearRegression
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import mean_squared_error
+    # # Importe LinearRegression
+    # # Importe train_test_split
+    # # Importe mean_squared_error
+    # from sklearn.linear_model import LinearRegression
+    # from sklearn.model_selection import train_test_split
+    # from sklearn.metrics import mean_squared_error
 
-    # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("gm_2008_region.csv")
+    # # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
+    # df = pd.read_csv("gm_2008_region.csv")
 
-    # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = df["fertility"].values
+    # # Asigne a la variable los valores de la columna `fertility`
+    # X_fertility = df["fertility"].values
 
-    # Asigne a la variable los valores de la columna `life`
-    y_life = df["life"].values
+    # # Asigne a la variable los valores de la columna `life`
+    # y_life = df["life"].values
 
-    # Divida los datos de entrenamiento y prueba. La semilla del generador de números
-    # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
-    (X_train, X_test, y_train, y_test,) = train_test_split(
-        X_fertility.reshape(-1, 1),
-        y_life,
-        test_size=0.2,
-        random_state=53,
-    )
+    # # Divida los datos de entrenamiento y prueba. La semilla del generador de números
+    # # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
+    # (X_train, X_test, y_train, y_test,) = train_test_split(
+    #     X_fertility.reshape(-1, 1),
+    #     y_life,
+    #     test_size=0.2,
+    #     random_state=53,
+    # )
 
-    # Cree una instancia del modelo de regresión lineal
-    linearRegression = LinearRegression()
+    # # Cree una instancia del modelo de regresión lineal
+    # linearRegression = LinearRegression()
 
-    # Entrene el clasificador usando X_train y y_train
-    linearRegression.fit(X_train, y_train)
+    # # Entrene el clasificador usando X_train y y_train
+    # linearRegression.fit(X_train, y_train)
 
-    # Pronostique y_test usando X_test
-    y_pred = linearRegression.predict(X_test)
+    # # Pronostique y_test usando X_test
+    # y_pred = linearRegression.predict(X_test)
 
-    # Compute and print R^2 and RMSE
-    print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    print("Root Mean Squared Error: {:6.4f}".format(rmse))
+    # # Compute and print R^2 and RMSE
+    # print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
+    # rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    # print("Root Mean Squared Error: {:6.4f}".format(rmse))
